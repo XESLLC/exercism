e175c65cc62cbd5a41d194b681737bd79e1986ea
David Abramowitz <david.xesllc@gmail.com>
2016-05-26 16:18:35 -0600

    finished Bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,12 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  input = input.replace(/\s/g, "");
+  if (input === input.toUpperCase() && input.match(/[a-z]/i) ) {return "Whoa, chill out!"}
+  if (input.slice(-1) === "?") {return "Sure."}
+  if (!input.match(/[a-z]/i) && !input === "") {return "Whatever."}
+  if (input === "") {return "Fine. Be that way!"}
+  return "Whatever."
 };
 
 module.exports = Bob;
